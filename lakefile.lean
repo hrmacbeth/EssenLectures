086a9a81ec1f8bc6045a7aa20d36a32abda535ad
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-02-28 19:31:15 -0500

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,34 @@
+import Lake
+open Lake DSL
+
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-Dpp.proofs.withType=false"
+]
+
+-- These settings only apply during `lake build`, but not in VSCode editor.
+def moreLeanArgs := moreServerArgs
+
+-- These are additional settings which do not affect the lake hash,
+-- so they can be enabled in CI and disabled locally or vice versa.
+-- Warning: Do not put any options here that actually change the olean files,
+-- or inconsistent behavior may result
+def weakLeanArgs : Array String :=
+  if get_config? CI |>.isSome then
+    #["-DwarningAsError=true"]
+  else
+    #[]
+
+package «gromov-hyperbolicity» where
+  moreServerArgs := moreServerArgs
+  -- add any package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+require Duper from git "https://github.com/leanprover-community/duper" @ "main"
+
+@[default_target]
+lean_lib «GromovHyperbolicity» where
+  moreLeanArgs := moreLeanArgs
+  weakLeanArgs := weakLeanArgs
+  -- add any library configuration options here
