606c748c319127b37f12baa0969d2a2d6bac0051
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-04-21 17:25:42 -0400

    drop old proofs in quasiconvex

--- GromovHyperbolicity/Quasiconvex.lean
+++ GromovHyperbolicity/Quasiconvex.lean
@@ -3,10 +3,6 @@
 import GromovHyperbolicity.GromovHyperbolic.Basic
 import Mathlib.Tactic.Peel
 
--- imports "HOL-Decision_Procs.Approximation"
--- hide_const (open) Approximation.Min
--- hide_const (open) Approximation.Max
-
 /-! # Quasiconvexity -/
 
 open Metric Set
@@ -14,11 +10,11 @@ open Metric Set
 variable {M : Type*} [MetricSpace M]
 
 /-- In a Gromov-hyperbolic setting, convexity is not a well-defined notion as everything should
-be coarse. The good replacement is quasi-convexity: A set $X$ is $C$-quasi-convex if any pair of
-points in $X$ can be joined by a geodesic that remains within distance $C$ of $X$. One could also
-require this for all geodesics, up to changing $C$, as two geodesics between the same endpoints
+be coarse. The good replacement is quasi-convexity: A set `x` is `C`-quasi-convex if any pair of
+points in `x` can be joined by a geodesic that remains within distance `C` of `x`. One could also
+require this for all geodesics, up to changing `C`, as two geodesics between the same endpoints
 remain within uniformly bounded distance. We use the first definition to ensure that a geodesic is
-$0$-quasi-convex. -/
+0-quasi-convex. -/
 def quasiconvex (C : ℝ) (X : Set M) : Prop :=
   C ≥ 0 ∧ (∀ x ∈ X, ∀ y ∈ X, ∃ G, geodesic_segment_between G x y ∧ (∀ z ∈ G, infDist z X ≤ C))
 
@@ -27,17 +23,14 @@ variable {C D : ℝ} {X G : Set M}
 lemma quasiconvexD (h : quasiconvex C X) {x y : M} (hx : x ∈ X) (hy : y∈X) :
     ∃ G, geodesic_segment_between G x y ∧ (∀ z ∈ G, infDist z X ≤ C) := by
   aesop (add norm unfold quasiconvex)
--- using assms unfolding quasiconvex_def by auto
 
 lemma quasiconvexC (h : quasiconvex C X) : C ≥ 0 := by
   aesop (add norm unfold quasiconvex)
--- using assms unfolding quasiconvex_def by auto
 
 lemma quasiconvexI (hC : C ≥ 0)
     (hCX : ∀ x y, x ∈ X → y ∈ X → (∃ G, geodesic_segment_between G x y ∧ (∀ z ∈ G, infDist z X ≤ C))) :
     quasiconvex C X := by
   aesop (add norm unfold quasiconvex)
--- using assms unfolding quasiconvex_def by auto
 
 lemma quasiconvex_of_geodesic {G : Set M} (hG : geodesic_segment G) : quasiconvex 0 G := by
   apply quasiconvexI
@@ -52,21 +45,19 @@ lemma quasiconvex_of_geodesic {G : Set M} (hG : geodesic_segment G) : quasiconve
 
 lemma quasiconvex_empty (hC : C ≥ 0) : quasiconvex C (∅ : Set M) := by
   aesop (add norm unfold quasiconvex)
--- unfolding quasiconvex_def using assms by auto
 
 lemma quasiconvex_mono (hCD : C ≤ D) (hC : quasiconvex C G) : quasiconvex D G := by
   obtain ⟨h1, h2⟩ := hC
   refine ⟨by linarith, ?_⟩
   peel h2
   linarith
--- using assms unfolding quasiconvex_def by (auto, fastforce)
 
 variable [Gromov_hyperbolic_space M] [GeodesicSpace M]
 
 local notation "δ" => Gromov_hyperbolic_space.deltaG M
 
-/-- The $r$-neighborhood of a quasi-convex set is still quasi-convex in a hyperbolic space,
-for a constant that does not depend on $r$. -/
+/-- The `r`-neighborhood of a quasi-convex set is still quasi-convex in a hyperbolic space,
+for a constant that does not depend on `r`. -/
 lemma quasiconvex_thickening [Inhabited M] (h : quasiconvex C X) (hr : r ≥ 0) :
     quasiconvex (C + 8 * δ) (⋃ x ∈ X, closedBall x r) := by
   classical
@@ -141,8 +132,8 @@ lemma quasiconvex_thickening [Inhabited M] (h : quasiconvex C X) (hr : r ≥ 0) 
 --     apply (rule exI[of _ "{y--z}"]) using A by auto
 -- qed
 
-/-- If $x$ has a projection $p$ on a quasi-convex set $G$, then all segments from a point in $G$
-to $x$ go close to $p$, i.e., the triangular inequality $d(x,y) ≤ d(x,p) + d(p,y)$ is essentially
+/-- If `x` has a projection `p` on a quasi-convex set `G`, then all segments from a point in `G`
+to `x` go close to `p`, i.e., the triangular inequality $d(x,y) ≤ d(x,p) + d(p,y)$ is essentially
 an equality, up to an additive constant. -/
 lemma dist_along_quasiconvex (hCG : quasiconvex C G) (hp : p ∈ proj_set x G) (hy : y ∈ G) :
     dist x p + dist p y ≤ dist x y + 4 * δ + 2 * C := by
@@ -187,7 +178,7 @@ lemma proj_along_quasiconvex_contraction (h : quasiconvex C G) (hx : px ∈ proj
   obtain _ | _ := max_cases (dist x y + dist px py) (dist px x + dist py y) <;>
   linarith
 
-/-- The projection on a quasi-convex set is $1$-Lipschitz up to an additive error. -/
+/-- The projection on a quasi-convex set is 1-Lipschitz up to an additive error. -/
 lemma proj_along_quasiconvex_contraction' (h : quasiconvex C G) (hx : px ∈ proj_set x G)
     (hy : py ∈ proj_set y G) :
     dist px py ≤ dist x y + 4 * δ + 2 * C := by
@@ -197,30 +188,14 @@ lemma proj_along_quasiconvex_contraction' (h : quasiconvex C G) (hx : px ∈ pro
   have := dist_triangle y x px
   simp only [dist_comm] at *
   linarith
--- proof (cases "dist y py ≤ dist x px")
---   case True
---   have "dist x px + dist px py ≤ dist x py + 4 * δ + 2 * C"
---     by (rule dist_along_quasiconvex[OF assms(1) assms(2) proj_setD(1)[OF assms(3)]])
---   also have "... ≤ (dist x y + dist y py) + 4 * δ + 2 * C"
---     by (intro mono_intros)
---   finally show ?thesis using True by auto
--- next
---   case False
---   have "dist y py + dist py px ≤ dist y px + 4 * δ + 2 * C"
---     by (rule dist_along_quasiconvex[OF assms(1) assms(3) proj_setD(1)[OF assms(2)]])
---   also have "... ≤ (dist y x + dist x px) + 4 * δ + 2 * C"
---     by (intro mono_intros)
---   finally show ?thesis using False by (auto simp add: metric_space_class.dist_commute)
--- qed
 
 /-- We can in particular specialize the previous statements to geodesics, which are
-$0$-quasi-convex. -/
+0-quasi-convex. -/
 lemma dist_along_geodesic (h : geodesic_segment G) (hx : p ∈ proj_set x G) (hy : y ∈ G) :
     dist x p + dist p y ≤ dist x y + 4 * δ := by
   have H := dist_along_quasiconvex (quasiconvex_of_geodesic h) hx hy
   ring_nf at H ⊢
   exact H
--- using dist_along_quasiconvex[OF quasiconvex_of_geodesic[OF assms(1)] assms(2) assms(3)] by auto
 
 lemma proj_along_geodesic_contraction (h : geodesic_segment G) (hx : px ∈ proj_set x G)
     (hy : py ∈ proj_set y G) :
@@ -228,7 +203,6 @@ lemma proj_along_geodesic_contraction (h : geodesic_segment G) (hx : px ∈ proj
   have H := proj_along_quasiconvex_contraction (quasiconvex_of_geodesic h) hx hy
   ring_nf at H ⊢
   exact H
--- using proj_along_quasiconvex_contraction[OF quasiconvex_of_geodesic[OF assms(1)] assms(2) assms(3)] by auto
 
 lemma proj_along_geodesic_contraction' (h : geodesic_segment G) (hx : px ∈ proj_set x G)
     (hy : py ∈ proj_set y G) :
@@ -236,7 +210,6 @@ lemma proj_along_geodesic_contraction' (h : geodesic_segment G) (hx : px ∈ pro
   have H := proj_along_quasiconvex_contraction' (quasiconvex_of_geodesic h) hx hy
   ring_nf at H ⊢
   exact H
--- using proj_along_quasiconvex_contraction'[OF quasiconvex_of_geodesic[OF assms(1)] assms(2) assms(3)] by auto
 
 open Topology
 
